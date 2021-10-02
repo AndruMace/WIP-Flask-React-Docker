80dac0f21f954f8ce6f78f65d52d5ca5b0bab5bd
Andru Mace <andru@localhost.localdomain>
2021-10-02 11:52:00 -0400

    Added beginnings of user registration. Established 2-way comms with server and client.

--- api/server/api/views.py
+++ api/server/api/views.py
@@ -2,7 +2,7 @@ import os
 
 from flask import Blueprint
 from flask import __version__
-from flask import render_template
+from flask import request
 
 from server.extensions import db
 from server.initializers import redis
@@ -13,7 +13,17 @@ api = Blueprint("api", __name__)
 
 @api.get("/")
 def home():
-    return {'res' : 'Success'}
+    return {'Success' : 'API Hit'}
+
+@api.post("/register")
+def register():
+    user_information = request.json
+    print(user_information)
+
+    if (user_information is not None):
+        return {'Success' : user_information}
+    else:
+        return {'Error' : 'no data'}
 
 @api.get("/up")
 def up():
--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,68 @@
 import { useState, useEffect } from 'react'
 function App() {
   const [res, setRes] = useState({'res' : 'nothing yet'})
+  const [user, setUser] = useState({'fname' : 'John', 'lname' : 'Doe'})
 
   useEffect(() => {
     fetch('http://localhost:5000/')
       .then(res => res.json())
-      .then(data => setRes({'res': data.res}))
+      .then(data => setRes(data))
+      .catch((err) => {
+        setRes({'Error' : err})
+      })
   }, []);  
+
+  function updateInput(e) {
+    if(e.target.id === 'fname') {
+      setUser((oldUser) => { return {'fname': e.target.value, 'lname': oldUser.lname} }) 
+    } else {
+      setUser((oldUser) => { return {'fname': oldUser.fname, 'lname': e.target.value} })
+    }
+  }
+
+  function onSubmit(e) {
+    e.preventDefault()
+
+    fetch('http://localhost:5000/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    })
+    .then(response => response.json())
+    .then(data => {
+      setRes(data);
+      setUser({'fname' : '', 'lname' : ''})
+    })
+    .catch((error) => {
+      setRes(error);
+    });
+  }
+
+  function renderResponse(data) { 
+    if (data['Error']) {
+      return `Server returned and error. Error is as follows: ${data.Error}`
+    } else if (data['Success']) {
+      return `Server response successful. Here is the data returned: ${JSON.stringify(data.Success)}`
+    } else {
+      return `Server responded in a way that was not anticipated. Response is as follows: ${JSON.stringify(data)}`
+    }
+  }
+
   return (
-    <div className="App">
-      Data from server: {res.res}
+    <div className="App" style={{padding: '50px', backgroundColor: '#00004d', color: 'white', width: '100vw', height: '100vh' }}>
+      {renderResponse(res)}
+      <div>
+        <h2>Test Form</h2>
+        <form onSubmit={onSubmit}>
+          <label for="fname">First name:</label>
+          <input type="text" id="fname" name="fname" onChange={updateInput} value={user.fname}/>
+          <label for="lname">Last name:</label>
+          <input type="text" id="lname" name="lname" onChange={updateInput} value={user.lname}/>
+          <input type="submit" value="Submit"/>
+        </form>
+      </div>
     </div>
   );
 }
